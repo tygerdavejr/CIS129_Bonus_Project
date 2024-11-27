8d61d566b87c5d5572fbae56431fdb06036f8b41
David Vance <dvance4@mail.pima.edu>
2024-11-27 08:37:28 -0700

    Add files via upload
    Header/Placeholder for future code.

---
+++ CIS129_Vance-Huetter_Bonus.py
@@ -0,0 +1,9 @@
+# BONUS PROJECT - STUDENT REGISTRATIONS
+#
+# David Vance and Ty Huetter
+# Professor Kevin Chang
+# CIS129 - Programming and Problemsolving I
+# 18 October 2024
+"""This module allows a student to register for classes, checks for
+conflicts in scheduling, and then saves the resultant registration to 
+a file for future processing."""
